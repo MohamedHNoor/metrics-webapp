13f048834bcbabc1338b17051b30406302351ba4
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-02-06 19:57:11 +0200

    Connect store to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-extraneous-dependencies */
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import CountryDetalis from './components/country/Country';
+import Header from './components/header/Header';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header>Most Views</Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country/:id" element={<CountryDetalis />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

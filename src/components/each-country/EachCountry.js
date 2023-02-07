bb865820e9d7d08c59e33ae7b2d88ab97514fe98
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-02-07 11:46:19 +0200

    Add right arrow

--- src/components/each-country/EachCountry.js
+++ src/components/each-country/EachCountry.js
@@ -1,17 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 import './eachCountry.styles.css';
 
 const eachCountry = (props) => {
-  const {
-    id, name, image, timezone,
-  } = props;
+  const { id, name, image, timezone } = props;
   return (
     <div className="each-country">
       <NavLink to={`/country/${id}`} key={id}>
         <div className="list">
           <h2>{name}</h2>
-          <p>right arrow</p>
+          <AiOutlineArrowRight className="righ-arrow" />
         </div>
         <div>
           <img className="flag-image" src={image} alt="Flag" />
